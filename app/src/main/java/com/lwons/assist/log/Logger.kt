026ae1171e06b4ee799ead474bc8268cfac3ddd8
liuwons <liuwons@163.com>
2024-02-25 20:25:39 +0800

    [feat] log level

--- app/src/main/java/com/lwons/assist/log/Logger.kt
+++ app/src/main/java/com/lwons/assist/log/Logger.kt
@@ -3,19 +3,38 @@ package com.lwons.assist.log
 import android.util.Log
 
 object Logger {
+    private var level: LogLevel = LogLevel.DEBUG
+
     fun d(tag: String, msg: String) {
-        Log.d(tag, msg)
+        if (level == LogLevel.DEBUG) {
+            Log.d(tag, msg)
+        }
     }
 
     fun i(tag: String, msg: String) {
-        Log.i(tag, msg)
+        if (level >= LogLevel.INFO) {
+            Log.i(tag, msg)
+        }
     }
 
     fun w(tag: String, msg: String) {
-        Log.w(tag, msg)
+        if (level >= LogLevel.WARN) {
+            Log.w(tag, msg)
+        }
     }
 
     fun e(tag: String, msg: String) {
-        Log.e(tag, msg)
+        if (level >= LogLevel.ERROR) {
+            Log.e(tag, msg)
+        }
     }
+
+}
+
+enum class LogLevel(value: Int) {
+    DEBUG(0),
+    INFO(1),
+    WARN(2),
+    ERROR(3)
+
 }
