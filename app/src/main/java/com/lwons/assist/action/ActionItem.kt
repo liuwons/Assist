42c85cea8304abb3a1c0c56c2c3f8f109887ab23
liuwons <liuwons@163.com>
2024-02-26 10:49:17 +0800

    [ui] item ui

--- app/src/main/java/com/lwons/assist/action/ActionItem.kt
+++ app/src/main/java/com/lwons/assist/action/ActionItem.kt
@@ -2,18 +2,21 @@ package com.lwons.assist.action
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun ActionItem(action: AssistAction, modifier: Modifier = Modifier) {
     Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
         Spacer(modifier = Modifier.weight(1f))
         Icon(painterResource(id = action.icon(ActionExecutor.state(action.action).value)), contentDescription = action.name)
+        Spacer(modifier = Modifier.height(8.dp))
         Text(text = action.name)
         Spacer(modifier = Modifier.weight(1f))
     }
